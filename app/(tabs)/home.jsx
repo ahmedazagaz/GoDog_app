89a8fcfad2785ea88a50e97887811e92ef9309c7
ahmedazagaz <contactwithazagaz@gmail.com>
2024-08-21 01:40:09 +0100

    update home

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,42 @@
-import { View, Text } from "react-native";
+import { Text, FlatList, View, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../../constants";
 
 const Home = () => {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView className="bg-neutral-700">
+            <FlatList 
+                data={[{id:1},{id:2},{id:3}]}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({item}) => (
+                    <Text className="text-3xl text-white">{item.id}</Text>
+                )}
+                ListHeaderComponent={() => (
+                    <View className="my-6 px-4 space-y-6">
+                        <View className="justify-between items-start flex-row mb-6">
+                            <View>
+                                <Text className="font-pmedium text-sm text-gray-100">
+                                    Welcome👋
+                                </Text>
+                                <Text className="text-2xl font-psemibold text-white">
+                                    Ahmed
+                                </Text>
+                            </View>
+
+                            <View className="mt-1.5">
+                                <Image
+                                    source={images.logoSmall}
+                                    className="w-10 h-12"
+                                    resizeMode="contain"
+                                />
+                            </View>
+                        </View>
+                    </View>
+                )}
+            />  
+        </SafeAreaView>
     );
 };
 
