2f12df7ad86895c13889adfa8f0e93a4618335d8
ahmedazagaz <contactwithazagaz@gmail.com>
2024-08-27 15:23:39 +0100

    Lessons Screen

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,18 +4,56 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "../../constants";
 
+const articles = [
+    {
+        id: 1,
+        title: "Training Markers: 3 Ways To Help Your Dog Understand You",
+        image: images.trainingMarkers,
+    },
+    {
+        id: 2,
+        title: "Top 3 ways to motivate Your Dog",
+        image: images.motivateDog,
+    },
+    {
+        id: 3,
+        title: "Successful Training: 4 Steps You Shouldn't Skip",
+        image: images.successfulTraining,
+    },
+    {
+        id: 4,
+        title: "TOP 13 Mistakes to avoid When Training Dogs",
+        image: images.trainingMistakes,
+    },
+];
+
 const Home = () => {
     return (
         <SafeAreaView className="bg-neutral-700">
             <FlatList 
-                data={[{id:1},{id:2},{id:3}]}
+                data={articles}
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({item}) => (
-                    <Text className="text-3xl text-white">{item.id}</Text>
+                    <View className="my-3 px-3">
+                        {/* Ligne de séparation */}
+                        <View className="h-0.5 bg-[#484848] mb-2" />
+
+                        <View className="flex-row items-center">
+                            <Image
+                                source={item.image}
+                                className="w-12 h-12 rounded border-2"
+                                style={{ borderColor: '#D0BEA0' }} 
+                                resizeMode="contain"
+                            />
+                            <Text className="text-white text-base font-psemibold ml-3 flex-shrink">
+                                {item.title}
+                            </Text>
+                        </View>
+                    </View>
                 )}
                 ListHeaderComponent={() => (
                     <View className="my-6 px-4 space-y-6">
-                        <View className="justify-between items-start flex-row mb-6">
+                        <View className="justify-between items-start flex-row mb-5">
                             <View>
                                 <Text className="font-pmedium text-sm text-gray-100">
                                     Welcome👋
@@ -33,6 +71,10 @@ const Home = () => {
                                 />
                             </View>
                         </View>
+
+                        <Text className="text-zinc-200 text-lg font-psemibold">
+                            Essential
+                        </Text>
                     </View>
                 )}
             />  
--- constants/images.js
+++ constants/images.js
@@ -5,5 +5,9 @@ import path from "../assets/images/path.png";
 import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
+import successfulTraining from "../assets/images/successfulTraining.jpg";
+import trainingMarkers from "../assets/images/trainingMarkers.jpg";
+import trainingMistakes from "../assets/images/trainingMistakes.jpg";
+import motivateDog from "../assets/images/motivateDog.png";
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
+export default { profile, thumbnail, cards, path, logo, logoSmall, empty,successfulTraining,trainingMistakes,trainingMarkers,motivateDog };
