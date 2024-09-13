9b81321674c36dcc64e748351e1c70e7bc7bca30
ahmedazagaz <contactwithazagaz@gmail.com>
2024-09-13 18:22:49 +0100

    update

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,11 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { useLocalSearchParams } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Search = () => {
+    const {query} = useLocalSearchParams();
     return (
-        <view>
-            <Text>Search</Text>
-        </view>
+        <SafeAreaView className="bg-neutral-700  h-full">
+            <Text className="text-3xl text-white">{query}</Text>
+        </SafeAreaView>
     )
 }
 
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,38 +1,42 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
-
+import { View, TextInput, TouchableOpacity, Image, Alert } from 'react-native';
 import { icons } from '../constants';
+import { router, usePathname } from 'expo-router';
 
-const SearchInput = ({
-  title,
-  value,
-  placeholder,
-  handleChangeText,
-  otherStyles,
-}) => {
-  const [showPassword, setShowPassword] = useState(false);
+const SearchInput = () => {
+  const pathname = usePathname();
+  const [query, setQuery] = useState('');
 
   return (
-      <View className="border-2 border-gray-600 w-full h-16 px-4 bg-neutral-700 rounded-2xl focus:border-orange-100 flex-row items-center space-x-4">
-        <TextInput
-          className="text-base mt-0.5 text-white flex-1
-          font-pregular"
-          value={value}
-          placeholder="Search for a video topic"
-          placeholderTextColor="#7b7b8b"
-          onChangeText={handleChangeText}
-          secureTextEntry={title === "Password" && !showPassword}
-        />
+    <View className="border-2 border-gray-600 w-full h-16 px-4 bg-neutral-700 rounded-2xl focus:border-orange-100 flex-row items-center space-x-4">
+      <TextInput
+        className="text-base mt-0.5 text-white flex-1 font-pregular"
+        value={query}
+        placeholder="Search for a video topic"
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e) => setQuery(e)}
+      />
+      <TouchableOpacity
+        onPress={() => {
+          if (!query) {
+            return Alert.alert("Missing query", "Please input something to search results across database");
+          }
 
-      <TouchableOpacity>
+          if (pathname.startsWith("/search")) {
+            router.setParams({ query });
+          } else {
+            router.push(`/search/${query}`); // Utilisation des backticks pour interpolation
+          }
+        }}
+      >
         <Image
-        source={icons.search}
-        className="w-5 h-5"
-        resizeMode='contain'
+          source={icons.search}
+          className="w-5 h-5"
+          resizeMode="contain"
         />
       </TouchableOpacity>
-      </View>
+    </View>
   );
 };
 
-export default SearchInput ;
+export default SearchInput;
